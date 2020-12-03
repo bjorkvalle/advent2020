e38b6c331c3e42149ee6035e6b48dccc39272711
bjorkvalle <eric.bjorkvall@outlook.com>
2020-12-03 20:47:02 +0100

    Day one init

--- Advent2020.App/DemoManager.cs
+++ Advent2020.App/DemoManager.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Advent2020.App.DayOne;
+using System;
 
 namespace Advent2020.App
 {
@@ -39,7 +36,10 @@ Available projects
             switch ((Demos)selected)
             {
                 case Demos.ReportRepair:
-                    break;
+                    {
+                        new ReportRepair();
+                        break;
+                    }
                 default:
                     break;
             }
